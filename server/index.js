b0a7ca099e596a9e88bb1c198dd09253d5ac9a26
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2023-07-20 11:14:36 +0700

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDatabase();
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:4000","http://localhost:7000"];
+const allowedOrigins = ["https://mundesign.net", "https://adm.mundesign.net/","https://guest.mundesign.net/"];
 
 app.use(
   cors({
