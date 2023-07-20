64be65f8af5e59e524629a3fb3a38e22947104de
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2023-07-20 12:48:19 +0700

    update

--- admin/src/service/JobService.js
+++ admin/src/service/JobService.js
@@ -79,7 +79,7 @@ export const updateByGuest = async (id, data, access_token) => {
     Authorization: `Bearer ${access_token}`,
   };
   const res = axiosJWT.put(
-    `http://localhost:5000/api/v1/job/update/admin/${id}`,
+    `${URL}/api/v1/job/update/admin/${id}`,
     data,
     {
       headers,
