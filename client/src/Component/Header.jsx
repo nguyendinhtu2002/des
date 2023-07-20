727030297bc285562bd9de24924888ee9df534e7
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2023-07-20 16:58:01 +0700

    update

--- client/src/Component/Header.jsx
+++ client/src/Component/Header.jsx
@@ -36,10 +36,16 @@ function Header() {
         </Link>
 
         <div class="flex items-center md:order-2">
+          <span class="block text-sm text-gray-900 dark:text-white mr-2">
+            Số dư: {formattedAmount(userLogin?.money)}
+          </span>{" "}
           <button
             type="button"
             class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-            onClick={handleClick}
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom"
           >
             <span class="sr-only">Open user menu</span>
             <img
@@ -49,26 +55,19 @@ function Header() {
             />
           </button>
           <div
-            class={
-              click
-                ? " z-50 absolute m-[-40px] top-[50px] my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                : "hidden"
-            }
+            class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
             id="user-dropdown"
           >
-            <div class="px-4 py-3">
-              <span class="block text-sm text-gray-900 dark:text-white">
-                Số dư: {formattedAmount(userLogin?.money)}
-              </span>
-            </div>
-            <ul class="py-2">
+          
+            <ul class="py-2" aria-labelledby="user-menu-button">
               <li>
-                <button
+                <a
+                  href="#"
                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                   onClick={handleLogout}
                 >
                   Sign out
-                </button>
+                </a>
               </li>
             </ul>
           </div>
